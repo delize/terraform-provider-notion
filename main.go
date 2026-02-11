c82ef21b556d27aef526d4188ecbdb21b8ef050d
delize <4028612+delize@users.noreply.github.com>
2026-02-11 18:51:54 +0100

    Add release workflow and Terraform Registry manifest

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
+var version = "dev"
+
 func main() {
 	opts := providerserver.ServeOpts{
 		Address: "registry.terraform.io/andrew/notion",
 	}
 
-	err := providerserver.Serve(context.Background(), provider.New("dev"), opts)
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
